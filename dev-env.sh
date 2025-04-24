c7f0f13e318461c3312d99155148018367d5271f
maksmarinov <mksmarinov@gmail.com>
2025-04-24 14:17:40 +0300

    update scr

--- dev-env.sh
+++ dev-env.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-sudo add-apt-repository ppa:neovim-ppa/stable 
+
 sudo apt-get install software-properties-common
-sudo apt-get update
-sudo apt-get install
 sudo apt-get install python-dev python-pip python3-dev python3-pip
-sudo apt-get install -y git curl wget fuse build-essential
+sudo apt-get install -y git curl wget fuse build-essential gh
 sudo apt-get install -y tmux
-sudo apt-get install make git gh unzip gcc ripgrep xclip
 sudo add-apt-repository universe
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo apt update
+sudo apt install -y make gcc ripgrep unzip git xclip neovim
+sudo apt upgrade
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)"
 
 sudo apt install -y ./ghostty_*.deb
-sudo apt install -y neovim 
 sudo apt install -y fzf 
 # Download and install nvm:
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
 # in lieu of restarting the shell
 \. "$HOME/.nvm/nvm.sh"
@@ -34,7 +35,7 @@ echo "set -g default-terminal "tmux-256color"
 set -s escape-time 0
 set -g base-index 1
 unbind C-b
-set-option -g prefix C-ai
+set-option -g prefix C-a
 bind-key C-a send-prefix
 bind -r h select-pane -L
 bind -r j select-pane -D
@@ -46,9 +47,13 @@ echo "tmux conf created."
 echo "theme = IR_Black
 background-opacity = 0.9
 background-blur = 20
+maximize = true
 window-theme = ghostty
+window-decoration = none
 font-family= Fira Code
 font-size = 16">~/.config/ghostty/config
 
 echo "ghostty conf created."
 
+tmux source-file ~/.tmux.conf
+sudo apt update
